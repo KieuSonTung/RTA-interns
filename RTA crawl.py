7c6971af8b3010f59f9ac4202daa05ba665933c4
Son Tung <sontungkieu412@gmail.com>
2022-01-14 00:45:29 +0700

    Create RTA crawl.py

---
+++ RTA crawl.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+import re
+
+html_text = requests.get('https://doanhnghiep.biz/').text
+soup = bs(html_text, 'lxml')
+
+boxes = soup.find('div', class_='col-md-9')
+boxes = boxes.find_all('div')
+df = pd.DataFrame(columns=['name', 'mst', 'date', 'rep', 'address'])
+'''
+name: ten cong ty
+mst: ma so thue
+date: ngay hoat dong
+rep (representative): nguoi dai dien
+address: dia chi
+'''
+
+for box in boxes:
+    name = box.find('h6').find('a').string
+    mst = box.find('a', title=re.compile('Tra cứu Số ĐKKD/MST.*')).string
+    date = box.find(text=re.compile('Ngày hoạt động.*'))
+    date = re.findall(r'\d{2}/\d{2}/\d{4}', date.string)
+    rep = box.find('em').find('a').string
+    address = box.find('address')
+    address = address.get_text()
+    df = df.append({'name': name, 'mst': mst, 'date': date, 'rep': rep, 'address': address}, ignore_index=True)
+
+print(df.head())
+
+
+
+
