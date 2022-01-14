5c3c64f8e3f474cdf755de38f7d962d391d41018
Son Tung <sontungkieu412@gmail.com>
2022-01-14 14:51:16 +0700

    Change url

--- RTA crawl.py
+++ RTA crawl.py
@@ -3,7 +3,9 @@ import requests
 import pandas as pd
 import re
 
-html_text = requests.get('https://doanhnghiep.biz/').text
+base_url = 'https://doanhnghiep.biz/'
+
+html_text = requests.get(base_url).text
 soup = bs(html_text, 'lxml')
 
 boxes = soup.find('div', class_='col-md-9')
@@ -21,13 +23,16 @@ for box in boxes:
     name = box.find('h6').find('a').string
     mst = box.find('a', title=re.compile('Tra cứu Số ĐKKD/MST.*')).string
     date = box.find(text=re.compile('Ngày hoạt động.*'))
-    date = re.findall(r'\d{2}/\d{2}/\d{4}', date.string)
+    date = re.findall(r'\d{2}/\d{2}/\d{4}', date.string)[0]
     rep = box.find('em').find('a').string
     address = box.find('address')
     address = address.get_text()
     df = df.append({'name': name, 'mst': mst, 'date': date, 'rep': rep, 'address': address}, ignore_index=True)
 
-print(df.head())
+
+print(df)
+
+# df.to_csv('doanhnghiep_biz.csv', mode='a', index=False, header=False)
 
 
 
