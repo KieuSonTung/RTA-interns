891e17707797b876ce3860eeedad1f1bce3d08e5
Son Tung <sontungkieu412@gmail.com>
2022-01-14 16:38:30 +0700

    Update querying multiple pages

--- RTA crawl.py
+++ RTA crawl.py
@@ -3,13 +3,8 @@ import requests
 import pandas as pd
 import re
 
-base_url = 'https://doanhnghiep.biz/'
+# Create a dataframe to store data
 
-html_text = requests.get(base_url).text
-soup = bs(html_text, 'lxml')
-
-boxes = soup.find('div', class_='col-md-9')
-boxes = boxes.find_all('div')
 df = pd.DataFrame(columns=['name', 'mst', 'date', 'rep', 'address'])
 '''
 name: ten cong ty
@@ -19,26 +14,40 @@ rep (representative): nguoi dai dien
 address: dia chi
 '''
 
-for box in boxes:
-    name = box.find('h6').find('a').string
-    mst = box.find('a', title=re.compile('Tra cứu Số ĐKKD/MST.*')).string
-    date = box.find(text=re.compile('Ngày hoạt động.*'))
-    date = re.findall(r'\d{2}/\d{2}/\d{4}', date.string)[0]
-    rep = box.find('em').find('a').string
-    address = box.find('address')
-    address = address.get_text()
-    df = df.append({'name': name, 'mst': mst, 'date': date, 'rep': rep, 'address': address}, ignore_index=True)
+# Crawl
+base_url = 'https://doanhnghiep.biz/'
 
-<<<<<<< HEAD
-# print(df.head())
-=======
+url_list = ['{}?p={}'.format(base_url, str(page)) for page in range(1, 20)]
 
-print(df)
+try:
+    p = 1  # to track the pages
 
-# df.to_csv('doanhnghiep_biz.csv', mode='a', index=False, header=False)
->>>>>>> main
+    for url in url_list:
+        html_text = requests.get(url).text
+        soup = bs(html_text, 'lxml')
 
-print('Hello World')
+        boxes = soup.find('div', class_='col-md-9')
+        boxes = boxes.find_all('div')
 
+        for box in boxes:
+            name = box.find('h6').find('a').string
+            mst = box.find('a', title=re.compile('Tra cứu Số ĐKKD/MST.*')).string
+            date = box.find(text=re.compile('Ngày hoạt động.*'))
+            date = re.findall(r'\d{2}/\d{2}/\d{4}', date.string)[0]
+            rep = box.find('em').find('a').string
+            address = box.find('address')
+            address = address.get_text()
+            df = df.append({'name': name, 'mst': mst, 'date': date, 'rep': rep, 'address': address}, ignore_index=True)
 
+        print(df)
 
+        if p == 1:
+            df.to_csv('doanhnghiep_biz.csv', index=False)
+        else:
+            df.to_csv('doanhnghiep_biz.csv', mode='a', index=False, header=False)
+
+        print(p)
+        p += 1
+
+except:
+    pass
